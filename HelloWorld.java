2348c7b57b10efc1d2ad3dbc182f0d06541415bd
Umar Bisharat <muhammadumarjatt303@gmail.com>
2025-09-20 20:05:41 +0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// This program is the most basic Java program called "Hello World".
+// It shows how to print a simple message to the console.
+// This helps beginners understand the structure of a Java program 
+// including class definition, main method, and printing output.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print the message "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
